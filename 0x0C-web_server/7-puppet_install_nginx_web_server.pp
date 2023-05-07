01d2aa6d5e54f5db8f05151534c71d3eea16e2d2
Oluwatosin Stephen <oluwatosin.stephens@gmail.com>
2023-05-07 13:45:01 +0100

    updated code

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,42 +1,38 @@
-# Install Nginx
 class nginx {
   package { 'nginx':
     ensure => installed,
   }
 
-service { 'nginx':
-  ensure => running,
-  enable => true,
-}
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+  }
 
-# Configure Nginx
-file {'/etc/nginx/sites-available/default':
+  file { '/var/www/html/index.html':
+    content => 'Hello World!',
+  }
+
+  file { '/etc/nginx/sites-available/default':
     content => "
-    server {
-	listen 80;
-	server_name _;
-    	root /var/www/html;
-   	index index.html;
+server {
+  listen 80;
+  server_name _;
+  root /var/www/html;
+  index index.html;
 
-    location /redirect_me {
-        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-    }
+  location /redirect_me {
+    return 301 https://example.com;
+  }
 
-    location / {
-   	try_files $uri $uri/ =404;
-    }
- }",
+  location / {
+    try_files $uri $uri/ =404;
+  }
 }
+    ",
+  }
 
-# Create the default html page
-file { '/var/www/html/index.html':
-  content => 'Hello World!',
+  file { '/etc/nginx/sites-enabled/default':
+    ensure => link,
+    target => '/etc/nginx/sites-available/default',
+  }
 }
-
-file {'/etc/nginx/sites-enabled/default':
-  ensure => link,
-  target => '/etc/nginx/sites-available/default',
-}
-
-}
-
