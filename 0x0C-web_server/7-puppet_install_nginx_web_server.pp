12b1edea23639c85457d82e3df3bccdb2b15f272
Oluwatosin Stephen <oluwatosin.stephens@gmail.com>
2023-05-07 13:26:29 +0100

    updated code

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,72 +2,30 @@
 class { 'nginx': }
 
 # Configure Nginx
-file { '/etc/nginx/sites-available/default':
+file {'/etc/nginx/sites-available/default':
   ensure  => file,
   owner   => 'root',
   group   => 'root',
   content => "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    server_name 52.87.219.225;
+    listen 80;
+    server_name _;
+    root /var/www/html;
+    index index.html;
 
     location /redirect_me {
         return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
     }
-
-    error_page 404 /404.html;
-
-    location / {
-        try_files \$uri \$uri/ =404;
-    }
-
-    location = /404.html {
-        internal;
-        root /var/www/html;
-    }
-}",
-  notify  => Service['nginx'],
-}
+  }
 
 # Create the default html page
-file { '/var/www/html/default':
-  ensure  => file,
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0644',
-  content => "<!DOCTYPE html>
-<html>
-    <head>
-        <title>Welcome</title>
-    </head>
-    <body>
-         <p>Hello World!</p>
-    </body>
-</html>",
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
 }
 
-# Create the custom 404 error page
-file { '/var/www/html/404.html':
-  ensure  => file,
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0644',
-  content => "<!DOCTYPE html>
-<html>
-    <head>
-        <title>404 Not Found</title>
-    </head>
-    <body>
-        <h1>404 Not Found</h1>
-         <p>Ceci n'est pas une page</p>
-    </body>
-</html>",
+file {'/etc/nginx/sites-enabled/default':
+  ensure => link,
+  target => '/etc/nginx/sites-available/default',
 }
 
-# Start and enable the Nginx service
-service { 'nginx':
-  ensure  => running,
-  enable  => true,
-  require => File['/etc/nginx/sites-available/default'],
 }
 
