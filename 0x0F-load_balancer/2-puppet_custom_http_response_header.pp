edf71469ecd8fade7903f6be0adecebf7513c4de
Oluwatosin Stephen <oluwatosin.stephens@gmail.com>
2023-05-24 17:10:46 +0100

    task2 added

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,20 @@
+# Install nginx web server package
+package { 'nginx':
+  ensure => installed,
+}
+
+# Add custom header to the configuration file
+file_line { 'add_custom_header':
+  path    => '/etc/nginx/sites-available/default',
+  line    => '        add_header X-Served-By $hostname;',
+  match   => 'server {',
+  after   => true,
+  notify  => Service['nginx'],
+}
+
+# Ensure nginx is running and enabled
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => Package['nginx'],
+}
