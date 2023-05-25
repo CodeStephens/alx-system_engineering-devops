a13a8bc65a9c7fe79f37b442da1bed144b976196
Oluwatosin Stephen <oluwatosin.stephens@gmail.com>
2023-05-25 13:07:01 +0100

    task2 code updated

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -12,11 +12,12 @@ package { 'nginx':
 
 # Add custom header to the configuration file
 file_line { 'add_custom_header':
-  path   => '/etc/nginx/sites-available/default',
-  line   => "server {\n\tadd_header X-Served-By \$HOSTNAME;",
-  match  => '^server {',
-  after  => 'true',
-  notify => Service['nginx'],
+  path    => '/etc/nginx/sites-available/default',
+  line    => "server {\n\tadd_header X-Served-By \$HOSTNAME;",
+  match   => '^server {',
+  after   => 'true',
+  require => Package['nginx'],
+  notify  => Service['nginx'],
 }
 
 # Ensure nginx is running and enabled
