a4c40d090de2b3a0049c48464d457eff0664534e
CodeStephens <oluwatosin.stephens@gmail.com>
2023-06-04 00:53:51 +0100

    updated task5 code

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,39 +1,27 @@
-# Puppet manifest that installs and configures nginx
-class nginx {
-  package { 'nginx':
-    ensure => installed,
-  }
+# manifest installs nginx and configures the server
 
-  service { 'nginx':
-    ensure => running,
-    enable => true,
-  }
-
-  file { '/var/www/html/index.html':
-    content => 'Hello World!',
-  }
-
-  file { '/etc/nginx/sites-available/default':
-    content => "
-server {
-  listen 80;
-  server_name 52.87.219.225;
-  root /var/www/html;
-  index index.html;
-
-  location /redirect_me {
-    return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-  }
-
-  location / {
-    try_files $uri $uri/ =404;
-  }
+exec { 'apt-update':
+  command => '/usr/bin/apt-get update',
+  path    => '/usr/bin',
+  unless  => '/usr/bin/apt-get update -qq >/dev/null',
 }
-    ",
-  }
 
-  file { '/etc/nginx/sites-enabled/default':
-    ensure => link,
-    target => '/etc/nginx/sites-available/default',
-  }
+package { 'nginx':
+  ensure => installed,
+}
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => present,
+  content => "server {\n  listen 80 default_server;\n  listen [::]:80 default_server;\n  server_name _;\n\n  location /redirect_me {\n    return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\n  }\n\n  location / {\n    try_files \$uri \$uri/ =404;\n  }\n\n  error_page 404 /404.html;\n  location = /404.html {\n    internal;\n    root /var/www/html;\n  }\n}\n",
+  notify  => Service['nginx'],
+}
+
+file { '/var/www/html/404.html':
+  ensure  => present,
+  content => "<!DOCTYPE html>\n<html>\n  <head>\n    <title>404 Not Found</title>\n  </head>\n  <body>\n    <h1>404 Not Found</h1>\n    <p>Ceci n'est pas une page</p>\n  </body>\n</html>\n",
+}
+
+service { 'nginx':
+  ensure => running,
+  enable => true,
 }
