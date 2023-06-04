91dbb60e6d4ba62ee3f455e5f6c8ab2b44a1cec5
CodeStephens <oluwatosin.stephens@gmail.com>
2023-06-04 01:52:37 +0100

    updated task5 code

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -12,12 +12,6 @@ package { 'nginx':
   before => File['/etc/nginx/sites-available/default'],
 }
 
-file { '/etc/nginx/sites-available/default':
-  ensure  => present,
-  content => "server {\n  listen 80 default_server;\n  listen [::]:80 default_server;\n  server_name _;\n\n  location /redirect_me {\n    return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\n  }\n\n  location / {\n    try_files \$uri \$uri/ =404;\n  }\n\n  error_page 404 /404.html;\n  location = /404.html {\n    internal;\n    root /var/www/html;\n  }\n}\n",
-  before => File['/var/www/html/404.html'],
-}
-
 file { '/var/www/html/404.html':
   ensure  => present,
   content => "Ceci n'est pas une page",
@@ -27,5 +21,10 @@ file { '/var/www/html/404.html':
 service { 'nginx':
   ensure => running,
   enable => true,
-  -> File['/var/www/html/404.html'],
+  suscribe => File['/var/www/html/404.html'],
+} ->
+file { '/etc/nginx/sites-available/default':
+  ensure  => present,
+  content => "server {\n  listen 80 default_server;\n  listen [::]:80 default_server;\n  server_name _;\n\n  location /redirect_me {\n    return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\n  }\n\n  location / {\n    try_files \$uri \$uri/ =404;\n  }\n\n  error_page 404 /404.html;\n  location = /404.html {\n    internal;\n    root /var/www/html;\n  }\n}\n",
+  require => Package['nginx'],
 }
