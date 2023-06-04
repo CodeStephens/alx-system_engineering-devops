2aafd4765fc0b5af9a3d1fc185fabaa22c0b1a2d
CodeStephens <oluwatosin.stephens@gmail.com>
2023-06-04 01:06:05 +0100

    updated task5 code

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -8,17 +8,21 @@ exec { 'apt-update':
 
 package { 'nginx':
   ensure => installed,
+  -> Exec['apt-update'],
 }
 
 file { '/etc/nginx/sites-available/default':
   ensure  => present,
   content => "server {\n  listen 80 default_server;\n  listen [::]:80 default_server;\n  server_name _;\n\n  location /redirect_me {\n    return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\n  }\n\n  location / {\n    try_files \$uri \$uri/ =404;\n  }\n\n  error_page 404 /404.html;\n  location = /404.html {\n    internal;\n    root /var/www/html;\n  }\n}\n",
+  -> Pacakge['nginx'],
   notify  => Service['nginx'],
 }
 
 file { '/var/www/html/404.html':
   ensure  => present,
-  content => "<!DOCTYPE html>\n<html>\n  <head>\n    <title>404 Not Found</title>\n  </head>\n  <body>\n    <h1>404 Not Found</h1>\n    <p>Ceci n'est pas une page</p>\n  </body>\n</html>\n",
+  content => "Ceci n'est pas une page",
+  before => Service['nginx'],
+  require => Package['nginx'],
 }
 
 service { 'nginx':
