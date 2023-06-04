36aa29288e4497e2489220434b44eee56154b796
CodeStephens <oluwatosin.stephens@gmail.com>
2023-06-04 22:25:03 +0100

    updated task5 code

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -4,7 +4,7 @@ exec { 'apt-update':
   command => '/usr/bin/apt-get update',
   path    => '/usr/bin',
   unless  => '/usr/bin/apt-get update -qq >/dev/null',
-  before => Package['nginx'],
+  before  => Package['nginx'],
 }
 
 package { 'nginx':
@@ -15,15 +15,15 @@ package { 'nginx':
 file { '/var/www/html/404.html':
   ensure  => present,
   content => "Ceci n'est pas une page",
-  notify => Service['nginx'],
+  notify  => Service['nginx'],
 }
 
 service { 'nginx':
-  ensure => running,
-  enable => true,
+  ensure    => running,
+  enable    => true,
   subscribe => File['/var/www/html/404.html'],
-} ->
-file { '/etc/nginx/sites-available/default':
+}
+-> file { '/etc/nginx/sites-available/default':
   ensure  => present,
   content => "server {\n  listen 80 default_server;\n  listen [::]:80 default_server;\n  server_name _;\n\n  location /redirect_me {\n    return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\n  }\n\n  location / {\n    try_files \$uri \$uri/ =404;\n  }\n\n  error_page 404 /404.html;\n  location = /404.html {\n    internal;\n    root /var/www/html;\n  }\n}\n",
   require => Package['nginx'],
