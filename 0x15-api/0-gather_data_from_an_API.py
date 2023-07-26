31d1e602db4f3048f594549e282b6ee733840d34
CodeStephens <oluwatosin.stephens@gmail.com>
2023-07-26 18:55:39 +0100

    modified task0-gather data from an API code

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" Script pulls up data from a webpage using API """
+"""Script
+pulls up data from a webpage using API.
+"""
 
 import requests
 from sys import argv
@@ -7,7 +9,7 @@ from sys import argv
 
 def get_employee_todo_progress(employee_id):
     """ This
-    method functionality aggregates tasks performed by a given employee
+    method functionality aggregates tasks performed by a given employee.
     """
 
     base_url = "https://jsonplaceholder.typicode.com"
