9bf6ce59d8a149e9884548d012066cefe3949210
CodeStephens <oluwatosin.stephens@gmail.com>
2023-08-07 23:37:42 +0100

    modified task0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,9 +5,9 @@ from sys import argv
 
 
 def get_employee_todo_progress(employee_id):
-    """
+    '''
     This functionality aggregates tasks performed by a given employee.
-    """
+    '''
 
     base_url = "https://jsonplaceholder.typicode.com"
     user_url = f"{base_url}/users/{employee_id}"
